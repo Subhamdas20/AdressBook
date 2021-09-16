4a3a90f6402fcea4ef57a673410f9328249353c6
Subhamdas20 <subham21.sd@gmail.com>
2021-09-17 01:05:47 +0530

    Added a new contact to AddressBook

--- src/com/bz/addressbook/AddressBook.java
+++ src/com/bz/addressbook/AddressBook.java
@@ -1,5 +1,7 @@
 package com.bz.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBook {
 
     String firstName,lastName,address,city,state,emailId;
@@ -69,9 +71,33 @@ public class AddressBook {
     public void setPhoneNumber(int phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+    public void setData(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter  first name");
+        String firstName=sc.nextLine();
+        setFirstName(firstName);
+        System.out.println("Last name");
+        String lastName = sc.nextLine();
+        setLastName(lastName);
+        System.out.println("Enter address");
+        String address = sc.nextLine();
+        setAddress(address);
+        System.out.println("Enter city ");
+        String city = sc.nextLine();
+        setCity(city);
+        System.out.println("Enter state ");
+        String state = sc.nextLine();
+        setState(state);
+        System.out.println("Enter EmailId ");
+        String emailId = sc.nextLine();
+        setEmailId(emailId);
+        System.out.println("Enter zip ");
+        int zip  = sc.nextInt();
+        setZip(zip);
+        System.out.println("Enter phone number ");
+        int phoneNumber = sc.nextInt();
+        setPhoneNumber(phoneNumber);
 
-
-
-
+    }
 
     }
---
+++ src/com/bz/addressbook/Main.java
@@ -0,0 +1,14 @@
+package com.bz.addressbook;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AddressBook contact_One = new AddressBook();
+        AddressBook contact_Two = new AddressBook();
+        AddressBook contact_Three = new AddressBook();
+        contact_One.setData();
+        contact_Two.setData();
+    }
+}
